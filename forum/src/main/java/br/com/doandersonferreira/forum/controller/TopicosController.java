5c13878a6f4dace17cd086285e64e27a90a55302
Anderson Ferreira <doandersonferreira@gmail.com>
2020-05-28 20:10:36 -0300

    Adicionada a dependencia ao Devtools; criação e uso de classes DTO

---
+++ forum/src/main/java/br/com/doandersonferreira/forum/controller/TopicosController.java
@@ -0,0 +1,23 @@
+package br.com.doandersonferreira.forum.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.doandersonferreira.forum.controller.dto.TopicoDto;
+import br.com.doandersonferreira.forum.modelo.Curso;
+import br.com.doandersonferreira.forum.modelo.Topico;
+
+@RestController
+public class TopicosController {
+
+	@RequestMapping("/topicos")
+	public List<TopicoDto> lista(){		
+		Topico topico = new Topico("Dúvida", "Dúvida de Spring!", new Curso("Spring","Programação"));
+		
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+	}
+	
+}
