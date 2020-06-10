fab3bdaa077bc6b1c2e7828afb8a65187e79e484
Anderson Ferreira <doandersonferreira@gmail.com>
2020-06-10 18:15:37 -0300

    Gerando o JWT. Configuracoes a a partir do metodo gerarToken()

--- forum/src/main/java/br/com/doandersonferreira/forum/controller/form/LoginForm.java
+++ forum/src/main/java/br/com/doandersonferreira/forum/controller/form/LoginForm.java
@@ -1,5 +1,7 @@
 package br.com.doandersonferreira.forum.controller.form;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 public class LoginForm {
 
 	private String email;
@@ -16,6 +18,10 @@ public class LoginForm {
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	public UsernamePasswordAuthenticationToken converter() {
+		
+		return new UsernamePasswordAuthenticationToken(this.email, this.senha);
 	}
 	
 	
