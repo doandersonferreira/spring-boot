2eee6bf567bafd845e13e43cb4449c4dbc41f186
Anderson Ferreira <doandersonferreira@gmail.com>
2020-06-11 01:27:55 -0300

    Documentacao de API com Swagger Spring Fox

--- forum/src/main/java/br/com/doandersonferreira/forum/ForumApplication.java
+++ forum/src/main/java/br/com/doandersonferreira/forum/ForumApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableSpringDataWebSupport //  Dentre outras coisas, permite a injecao de uma instancia Pageable automaticamente em metodos do controller a partir de request parameters.
 @EnableCaching // Habilita o uso de cache na aplicacao
+@EnableSwagger2 // Habilita o Springfox
 public class ForumApplication {
 
 	public static void main(String[] args) {
